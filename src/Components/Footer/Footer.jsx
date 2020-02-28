398f675f5fef26adccb03b8f8a3669a29ffde7e0
George Kolovos <kolovos.giwrgos@gmail.com>
2020-02-28 16:38:13 +0200

    ChangeFooterColorAddPadding

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,16 +3,16 @@ import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
 const Footer = () => {
     return (
-        <MDBFooter color="purple darken-4" className="font-small pt-4">
-            <MDBContainer fluid className="text-center text-md-left">
+        <MDBFooter color="unique-color" className="font-small pt-4">
+            <MDBContainer fluid className="text-center">
                 <MDBRow>
-                    <MDBCol md="6">
+                    <MDBCol md="6" className="text-md-left pl-5">
                         <h5 className="title">Footer Header</h5>
                         <p>
                             Footer Content
                         </p>
                     </MDBCol>
-                    <MDBCol md="6">
+                    <MDBCol md="6" className="text-md-right pr-5">
                         <h5 className="title">Links</h5>
                         <ul>
                             <li className="list-unstyled">
@@ -33,7 +33,7 @@ const Footer = () => {
             </MDBContainer>
             <div className="footer-copyright text-center py-3">
                 <MDBContainer fluid>
-                    &copy; {new Date().getFullYear()} Copyright: <a href="https://sigfox.gr/"> Sigfox.gr </a>
+                    &copy; {new Date().getFullYear()} Copyright: <a href="https://sigfox.gr/">Sigfox.gr</a>
                 </MDBContainer>
             </div>
         </MDBFooter>

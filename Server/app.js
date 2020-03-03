952ae80137abd4a0c89f1e7e06b8aebf7bb92fa0
John <johnkyriakou94@gmail.com>
2020-03-03 14:21:53 +0200

    server

--- Server/app.js
+++ Server/app.js
@@ -2,7 +2,7 @@ const Express = require("express");
 const BodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
-
+const nodemailer = require('nodemailer');
 const CONNECTION_URL = "mongodb+srv://JGS:JGSJGS@jkcluster-ydbsz.mongodb.net/test?retryWrites=true&w=majority";
 const DATABASE_NAME = "sigfox-eshop-db";
 
@@ -27,6 +27,19 @@ app.listen(5000, () => {
     console.log("Connected to `" + DATABASE_NAME + "`!");
 });
 });
+
+
+
+let transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+     user: '938149de8b75bf',
+     pass: '9239e686261a09'
+  }
+});
+
+
 
 
 //////GET products/////////////////////////////////////
@@ -74,4 +87,20 @@ app.post("/partners", (request, response) => {
       }
       response.send(result.result);
   });
+  const message = {
+    from: 'elonmusk@tesla.com',
+    to: request.body.user.email,
+    subject: 'Design Your Model S | Tesla',
+    html: '<h1>Have the most fun you can in a car!</h1><p>Get your <b>Tesla</b> today!</p>'
+  };
+  
+  transport.sendMail(message, function(err, info) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(info);
+    }
+  });
+
+
 });

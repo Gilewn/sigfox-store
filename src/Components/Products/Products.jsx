642f93b506482519d86c73e9117bb2b24b3bc928
George Kolovos <kolovos.giwrgos@gmail.com>
2020-03-09 01:37:28 +0200

    AddProductsToTableTest

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -2,10 +2,107 @@ import React from "react";
 import Product from "../Product/Product";
 import SearchBox from '../SearchBox/SearchBox';
 import { Grid } from "@material-ui/core";
+import styled from 'styled-components'
+import { useTable } from 'react-table'
 
 import './Products.css';
 
+const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`
+
+function Table({ columns, data }) {
+    // Use the state and functions returned from useTable to build your UI
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = useTable({
+        columns,
+        data,
+    })
+
+    // Render the UI for your table
+    return (
+        <table {...getTableProps()}>
+            <thead>
+                {headerGroups.map(headerGroup => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map(column => (
+                            <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                        ))}
+                    </tr>
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row, i) => {
+                    prepareRow(row)
+                    return (
+                        <tr {...row.getRowProps()}>
+                            {row.cells.map(cell => {
+                                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                            })}
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    )
+}
+
 function Products(props) {
+    const columns = React.useMemo(
+        () => [
+            {
+                Header: 'Title',
+                columns: [
+                    {
+                        Header: 'Title',
+                        accessor: 'title',
+                    },
+                    {
+                        Header: 'Solution',
+                        accessor: 'solution',
+                    },
+                    {
+                        Header: 'Description',
+                        accessor: 'description',
+                    }
+                ],
+            }
+        ],
+        []
+    )
+
+    const data = props.items;
+
     return (
         <div style={{ marginTop: 20, padding: 30 }}>
             <SearchBox {...props} handleChange={props.handleChange} />
@@ -17,6 +114,9 @@ function Products(props) {
                     </Grid>
                 )}
             </Grid>
+            <Styles>
+                <Table columns={columns} data={data} />
+            </Styles>
         </div>
     )
 }

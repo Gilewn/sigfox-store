907fae5c0f2386bc2384e31453d72c8f752c7dab
George Kolovos <kolovos.giwrgos@gmail.com>
2020-04-03 23:59:15 +0300

    ModifyGroupByStyle

--- src/Components/GroupBy/GroupBy.jsx
+++ src/Components/GroupBy/GroupBy.jsx
@@ -30,14 +30,6 @@ function GroupBy(props) {
     return (
         <div className="GroupBy">
             <div className="dropdown">
-                <div className="dropdown-header-holder">
-                    <h1 className="dropdown-header">
-                        Group by
-                    </h1>
-                    <span>
-                        &#10149;
-                    </span>
-                </div>
                 <div className="dropdown-list-holder">
                     <ul className="dropdown-list">
                         {filteredValues.map((listTitle, index) =>
@@ -47,9 +39,7 @@ function GroupBy(props) {
                                 <a
                                     onClick={() => props.handleGroupBy(listTitle)}
                                     className="dropdown-list-item-anchor">
-                                    <text>
-                                        {listTitle.charAt(0).toUpperCase() + listTitle.slice(1)}
-                                    </text>
+                                    {listTitle.charAt(0).toUpperCase() + listTitle.slice(1)}
                                     <span>
                                         {orderSymbol}
                                     </span>

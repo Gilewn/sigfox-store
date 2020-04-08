ac45e0a33018323a19c81d2041f9c3960c310aa6
George Kolovos <kolovos.giwrgos@gmail.com>
2020-04-09 01:56:18 +0300

    AddOnClickGoToTopAfterRender

--- src/Components/ProductPage/ProductPage.jsx
+++ src/Components/ProductPage/ProductPage.jsx
@@ -1,38 +1,46 @@
-import React, { Component } from 'react';
-import ColumnOne from './ProductPageColumnOne';
-import ColumnTwo from './ProductPageColumnTwo';
-import axios from 'axios';
+import React, { Component } from "react";
+import ColumnOne from "./ProductPageColumnOne";
+import ColumnTwo from "./ProductPageColumnTwo";
+import axios from "axios";
 import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
-import './ProductPage.css';
+import "./ProductPage.css";
 
 class ProductPage extends Component {
-    state = {
-        product: [],
-        productimages: [],
-        productcertifications: []
-    }
+  state = {
+    product: [],
+    productimages: [],
+    productcertifications: [],
+  };
 
-    componentDidMount() {
-        axios.get(`http://localhost:5000/products/${this.props.match.params.id}`)
-            .then(res => {
-                console.log(res.data)
-                this.setState({ product: res.data });
-                this.setState({ productimages: res.data.images });
-                this.setState({ productcertifications: res.data.certifications });
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
+  componentDidMount() {
+    window.scrollTo(0, 0);
+    axios
+      .get(`http://localhost:5000/products/${this.props.match.params.id}`)
+      .then((res) => {
+        console.log(res.data);
+        this.setState({ product: res.data });
+        this.setState({ productimages: res.data.images });
+        this.setState({ productcertifications: res.data.certifications });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
-    render() {
-        // return <div className="productPage-container">
-        return  <ColumnOne product={this.state.product} album={this.state.productimages} certifications={this.state.productcertifications} />
-           
+  render() {
+    // return <div className="productPage-container">
+    return (
+      <ColumnOne
+        product={this.state.product}
+        album={this.state.productimages}
+        certifications={this.state.productcertifications}
+      />
+    );
 
-            {/* <ColumnTwo product={this.state.product} /> */}
-        
+    {
+      /* <ColumnTwo product={this.state.product} /> */
     }
+  }
 }
 
 export default ProductPage;
